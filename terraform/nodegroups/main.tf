68fea6a2455739eb3cf0c9e36aa8aee9586d21a9
ramkumarrc64-cell <ramkumarrc64@gmail.com>
2026-08-07 15:04:00 +0530

    changes

--- terraform/nodegroups/main.tf
+++ terraform/nodegroups/main.tf
@@ -1,111 +1,86 @@
 resource "aws_iam_role" "node" {
-
   name = "${var.cluster_name}-node-role"
 
-
   assume_role_policy = jsonencode({
-
     Version = "2012-10-17"
 
     Statement = [
       {
-
         Effect = "Allow"
 
         Principal = {
-
           Service = "ec2.amazonaws.com"
-
         }
 
         Action = "sts:AssumeRole"
-
       }
-
     ]
-
   })
-
 }
-
-
 
 resource "aws_iam_role_policy_attachment" "worker" {
-
-  role = aws_iam_role.node.name
-
+  role       = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-
 }
-
 
 resource "aws_iam_role_policy_attachment" "cni" {
-
-  role = aws_iam_role.node.name
-
+  role       = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-
 }
-
 
 resource "aws_iam_role_policy_attachment" "registry" {
-
-  role = aws_iam_role.node.name
-
+  role       = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-
 }
 
-
+# Optional but recommended for SSM access
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 
 resource "aws_eks_node_group" "nodes" {
-
-  cluster_name = var.cluster_name
-
+  cluster_name    = var.cluster_name
   node_group_name = "application-workers"
-
-
-  node_role_arn = aws_iam_role.node.arn
-
-
-  subnet_ids = var.subnet_ids
-
-
-  scaling_config {
-
-    desired_size = var.desired_nodes
-
-    max_size = var.max_nodes
-
-    min_size = var.min_nodes
-
-  }
-
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.subnet_ids
 
   instance_types = [
     var.node_instance_type
   ]
 
+  scaling_config {
+    desired_size = var.desired_nodes
+    min_size     = var.min_nodes
+    max_size     = var.max_nodes
+  }
+
+  capacity_type = "ON_DEMAND"
+
+  ami_type = "AL2_x86_64"
+
+  update_config {
+    max_unavailable = 1
+  }
 
   depends_on = [
-
     aws_iam_role_policy_attachment.worker,
-
     aws_iam_role_policy_attachment.cni,
-
-    aws_iam_role_policy_attachment.registry
-
+    aws_iam_role_policy_attachment.registry,
+    aws_iam_role_policy_attachment.ssm
   ]
 
+  tags = {
+    Name = "application-workers"
+  }
 }
 
-
 resource "aws_eks_access_entry" "nodes" {
-
-  cluster_name = var.cluster_name
-
+  cluster_name  = var.cluster_name
   principal_arn = aws_iam_role.node.arn
+  type          = "EC2_LINUX"
 
-  type = "EC2_LINUX"
-
+  depends_on = [
+    aws_eks_node_group.nodes
+  ]
 }
