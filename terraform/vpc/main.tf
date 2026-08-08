cc96c39d4852ca59876e2ce4204e46302a14c3ed
ramkumarrc64-cell <ramkumarrc64@gmail.com>
2026-08-08 13:12:08 +0530

    check

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,7 +1,5 @@
 resource "aws_vpc" "main" {
-
-  cidr_block = var.vpc_cidr
-
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -11,8 +9,11 @@ resource "aws_vpc" "main" {
 }
 
 
-resource "aws_internet_gateway" "igw" {
+# ---------------------------------------------------------
+# Internet Gateway
+# ---------------------------------------------------------
 
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
@@ -21,32 +22,35 @@ resource "aws_internet_gateway" "igw" {
 }
 
 
-resource "aws_subnet" "public" {
+# ---------------------------------------------------------
+# Public Subnets
+# ---------------------------------------------------------
 
+resource "aws_subnet" "public" {
   count = length(var.public_subnets)
 
-  vpc_id = aws_vpc.main.id
-
-  cidr_block = var.public_subnets[count.index]
-
-  availability_zone = var.availability_zones[count.index]
-
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.vpc_name}-public-${count.index + 1}"
+
+    "kubernetes.io/role/elb" = "1"
   }
 }
 
 
-resource "aws_subnet" "private" {
+# ---------------------------------------------------------
+# Private Subnets
+# ---------------------------------------------------------
 
+resource "aws_subnet" "private" {
   count = length(var.private_subnets)
 
-  vpc_id = aws_vpc.main.id
-
-  cidr_block = var.private_subnets[count.index]
-
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
   availability_zone = var.availability_zones[count.index]
 
   tags = {
@@ -57,6 +61,28 @@ resource "aws_subnet" "private" {
 }
 
 
+# ---------------------------------------------------------
+# Public Route Table
+# ---------------------------------------------------------
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+
+# ---------------------------------------------------------
+# Public Route Table Associations
+# ---------------------------------------------------------
+
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnets)
 
@@ -65,21 +91,11 @@ resource "aws_route_table_association" "public" {
 }
 
 
-resource "aws_route_table_association" "private" {
-  count = length(var.private_subnets)
-
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
-}
-
-
 # ---------------------------------------------------------
-# NAT Gateway (so private-subnet worker nodes have outbound
-# internet access to reach the EKS API endpoint, ECR, etc.)
+# NAT Gateway EIP
 # ---------------------------------------------------------
 
 resource "aws_eip" "nat" {
-
   domain = "vpc"
 
   tags = {
@@ -88,8 +104,11 @@ resource "aws_eip" "nat" {
 }
 
 
-resource "aws_nat_gateway" "nat" {
+# ---------------------------------------------------------
+# NAT Gateway
+# ---------------------------------------------------------
 
+resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
 
@@ -103,12 +122,14 @@ resource "aws_nat_gateway" "nat" {
 }
 
 
-resource "aws_route_table" "private" {
+# ---------------------------------------------------------
+# Private Route Table
+# ---------------------------------------------------------
 
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   }
@@ -119,13 +140,13 @@ resource "aws_route_table" "private" {
 }
 
 
+# ---------------------------------------------------------
+# Private Route Table Associations
+# ---------------------------------------------------------
+
 resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
 
-  count = length(aws_subnet.private)
-
-  subnet_id = aws_subnet.private[count.index].id
-
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
-
-
