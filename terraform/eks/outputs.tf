68335298332a19aa73a4716f338e4298a6969abb
ramkumarrc64-cell <ramkumarrc64@gmail.com>
2026-08-08 11:19:03 +0530

    4

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -1,23 +1,20 @@
-output "cluster_name" { 
-  description = "EKS Cluster Name" 
-  value = aws_eks_cluster.cluster.name 
-  } 
-  
-  output "cluster_endpoint" { 
-    
-    description = "EKS Cluster Endpoint" 
-    value = aws_eks_cluster.cluster.endpoint 
-    
-    } 
-    
-    output "cluster_certificate_authority_data" { 
-      
-      description = "EKS Cluster CA Data" 
-      value = aws_eks_cluster.cluster.certificate_authority[0].data 
-      
-      } 
+output "cluster_name" {
+  description = "EKS Cluster Name"
+  value       = aws_eks_cluster.cluster.name
+}
 
-output "cluster_arn" { 
-  description = "EKS Cluster ARN" 
-  value = aws_eks_cluster.cluster.arn 
-  }
+output "cluster_endpoint" {
+  description = "EKS Cluster Endpoint"
+  value       = aws_eks_cluster.cluster.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "EKS Cluster CA Data"
+  value       = aws_eks_cluster.cluster.certificate_authority[0].data
+}
+
+output "cluster_arn" {
+  description = "EKS Cluster ARN"
+  value       = aws_eks_cluster.cluster.arn
+}
+
--- terraform/nodegroups/outputs.tf
+++ terraform/nodegroups/outputs.tf
@@ -1,12 +1,10 @@
-output "node_group_name" { 
-  description = "EKS Node Group Name" 
-  value = aws_eks_node_group.nodes.node_group_name 
-  
-  } 
-
-
-output "node_role_arn" { 
-  
-description = "EKS Worker Node IAM Role ARN"
-value = aws_iam_role.node.arn 
+output "node_group_name" {
+  description = "EKS Node Group Name"
+  value       = aws_eks_node_group.nodes.node_group_name
 }
+
+output "node_role_arn" {
+  description = "EKS Worker Node IAM Role ARN"
+  value       = aws_iam_role.node.arn
+}
+
