790439fa67c0d5ad2fdaff0aef39662d6f131b86
ramkumarrc64-cell <ramkumarrc64@gmail.com>
2026-08-08 13:18:06 +0530

    check

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
 module "vpc" {
   source = "./vpc"
 
@@ -58,4 +63,16 @@ module "ecr" {
 
   bank_api_repository = var.bank_api_repository
   upi_api_repository  = var.upi_api_repository
+}
+
+# =========================================================
+# Terraform Backend
+# =========================================================
+
+terraform {
+  backend "s3" {
+    bucket = "ram-test-eks"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
