10228f01493b5686b18196428f03aff518208a5a
ramkumarrc64-cell <ramkumarrc64@gmail.com>
2026-08-08 12:18:21 +0530

    add

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -81,3 +81,59 @@ resource "aws_route_table_association" "public" {
 }
 
 
+# ---------------------------------------------------------
+# NAT Gateway (so private-subnet worker nodes have outbound
+# internet access to reach the EKS API endpoint, ECR, etc.)
+# ---------------------------------------------------------
+
+resource "aws_eip" "nat" {
+
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.vpc_name}-nat-eip"
+  }
+}
+
+
+resource "aws_nat_gateway" "nat" {
+
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.vpc_name}-nat"
+  }
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+}
+
+
+resource "aws_route_table" "private" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+
+
+resource "aws_route_table_association" "private" {
+
+  count = length(aws_subnet.private)
+
+  subnet_id = aws_subnet.private[count.index].id
+
+  route_table_id = aws_route_table.private.id
+}
+
+
