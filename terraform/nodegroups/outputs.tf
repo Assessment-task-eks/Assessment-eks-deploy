dac3e600e0295db3b1f05d9e26f27fef377006b6
ramkumarrc64-cell <ramkumarrc64@gmail.com>
2026-08-08 13:13:35 +0530

    add

--- terraform/nodegroups/outputs.tf
+++ terraform/nodegroups/outputs.tf
@@ -1,10 +1,24 @@
-output "node_group_name" {
-  description = "EKS Node Group Name"
-  value       = aws_eks_node_group.nodes.node_group_name
+output "bank_node_group_name" {
+  description = "Bank API EKS node group name"
+  value       = aws_eks_node_group.bank.node_group_name
+}
+
+output "bank_node_group_arn" {
+  description = "Bank API EKS node group ARN"
+  value       = aws_eks_node_group.bank.arn
+}
+
+output "upi_node_group_name" {
+  description = "UPI API EKS node group name"
+  value       = aws_eks_node_group.upi.node_group_name
+}
+
+output "upi_node_group_arn" {
+  description = "UPI API EKS node group ARN"
+  value       = aws_eks_node_group.upi.arn
 }
 
 output "node_role_arn" {
-  description = "EKS Worker Node IAM Role ARN"
+  description = "EKS worker node IAM role ARN"
   value       = aws_iam_role.node.arn
 }
-
